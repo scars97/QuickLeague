763eb126ed35657bc270c90608000dd5921a6edf
scars97 <qkr970504@gmail.com>
2023-05-15 20:51:04 +0900

    fixtures 화면출력

--- src/main/java/com/soccer/league/controller/PremierLeagueController.java
+++ src/main/java/com/soccer/league/controller/PremierLeagueController.java
@@ -3,12 +3,14 @@ package com.soccer.league.controller;
 import java.io.IOException;
 import java.util.List;
 
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.soccer.league.dto.FixturesDto;
 import com.soccer.league.dto.StandingsDto;
 import com.soccer.league.service.PremierLeagueService;
 
@@ -29,5 +31,16 @@ public class PremierLeagueController {
 
 		model.addAttribute("standings",standings);
 		return "epl/standings";
+	}
+	
+	@GetMapping("/fixtures/{leagueId}")
+	public String getFixtures(@PathVariable("leagueId") int leagueId, Model model) throws IOException, ParseException{
+		
+		List<FixturesDto> lastFixtures = premierLeagueService.getLastFixtures(leagueId);
+		List<FixturesDto> nextFixtures = premierLeagueService.getNextFixtures(leagueId);
+		
+		model.addAttribute("lastfixtures",lastFixtures);
+		model.addAttribute("nextfixtures",nextFixtures);
+		return "epl/fixtures";
 	}
 }
